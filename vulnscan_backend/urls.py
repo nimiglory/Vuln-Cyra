d781c6ce9fb27127350d0a17357a6ea9dc15c19b
nimiglory <nimiglory15@gmail.com>
2025-10-30 14:13:03 +0100

    added home page

--- vulnscan_backend/urls.py
+++ vulnscan_backend/urls.py
@@ -17,8 +17,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from scanner import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Vuln Cyra 🚀 Your backend is live!")
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/", include("scanner.urls")),  
     path("accounts/", include("allauth.urls")), 
