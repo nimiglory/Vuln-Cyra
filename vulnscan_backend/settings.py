c113f4aa6bda220fab1bb0c759c9cf79ce5a6c2b
nimiglory <nimiglory15@gmail.com>
2025-11-14 12:47:51 +0100

    final pls

--- vulnscan_backend/settings.py
+++ vulnscan_backend/settings.py
@@ -13,6 +13,7 @@ from decouple import config
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -156,16 +157,26 @@ WSGI_APPLICATION = 'vulnscan_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cyra_db',          # Your database name
+#         'USER': 'postgres',      # Your PostgreSQL user
+#         'PASSWORD': 'nimi',  # Replace with your postgres password
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cyra_db',          # Your database name
-        'USER': 'postgres',      # Your PostgreSQL user
-        'PASSWORD': 'nimi',  # Replace with your postgres password
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:nimi@localhost:5432/cyra_db'
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
